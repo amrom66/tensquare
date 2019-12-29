40afc6cb6122906abea10c2071f87b3241547b92
linjinbao66 <linjinbao66@gmail.com>
2019-12-29 21:18:50 +0800

    first commit

---
+++ tensquare_base/src/main/java/com/tensquare/base/BaseApplication.java
@@ -0,0 +1,21 @@
+package com.tensquare.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import util.IdWorker;
+
+/**
+ * 启动类
+ */
+@SpringBootApplication
+public class BaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BaseApplication.class);
+    }
+
+    @Bean
+    public IdWorker idWorker() {
+        return new IdWorker(1, 1);
+    }
+}
---
+++ tensquare_base/src/main/java/com/tensquare/base/service/LabelService.java
@@ -0,0 +1,60 @@
+package com.tensquare.base.service;
+
+import com.tensquare.base.dao.LabelDao;
+import com.tensquare.base.pojo.Label;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import util.IdWorker;
+
+import java.util.List;
+
+/**
+ * 标签业务逻辑类
+ */
+@Service
+public class LabelService {
+
+    @Autowired
+    private LabelDao labelDao;
+    @Autowired
+    private IdWorker idWorker;
+
+    /**
+     * 查询全部标签
+     * @return
+     */
+    public List<Label> findAll(){
+        return labelDao.findAll();
+    }
+    /**
+     * 根据ID查询标签
+     * @return
+     */
+    public Label findById(String id){
+        return labelDao.findById(id).get();
+    }
+
+    /**
+     * 增加标签
+     * @param label
+     */
+    public void add(Label label){
+        label.setId( idWorker.nextId()+"" );//设置ID
+        labelDao.save(label);
+    }
+    /**
+     * 修改标签
+     * @param label
+     */
+    public void update(Label label){
+        labelDao.save(label);
+    }
+    /**
+     * 删除标签
+     * @param id
+     */
+    public void deleteById(String id){
+        labelDao.deleteById(id);
+    }
+
+}
