da1f229191e466d16d05e9aa97e1923769ca3d13
linjinbao66 <linjinbao66@gmail.com>
2019-12-31 22:58:08 +0800

    提交 20191231

---
+++ tensquare_spit/src/test/java/tk/amrom/tensquare_spit/TensquareSpitApplicationTests.java
@@ -0,0 +1,13 @@
+package tk.amrom.tensquare_spit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TensquareSpitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
